19b955d3cb332bcd39cb5977aad487a3bb0abf0e
Caleb Forestal <133164966+Calebslc@users.noreply.github.com>
2023-09-20 14:18:15 -0800

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -50,7 +50,24 @@ export const challenge = async (args: string[]): Promise<string> => {
 };
 
 export const team = async (args: string[]): Promise<string> => {
-  return `Meet our creators at Relix:`;
+  return `Meet our creators at Relix:
+  
+             @@@@@@@@@@@@@                   sumfetch: summary display
+        @@@@               @@@@             -----------
+      @@                       @@            ABOUT
+    @@                           @@          ${config.name}
+  @@                               @@       ﰩ ${config.ps1_hostname}
+ @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@                                   @@     -----------
+@@             .@@@@@@@@@@.          @@      CONTACT 
+ @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+     @@@                        @@@         -----------
+        @@@                  @@@ @@          
+         @|  @@@@@@@@@@@@@@@@   @@           
+         @|                      @@         `;
 };
 
 const facts = ["Relix was named after a combination of 'Retro' and 'Helix'.", "The first line of code at Relix was written at 3:33 am."];
